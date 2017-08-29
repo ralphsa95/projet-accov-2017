ca33c004f1a0b657afdec9da9e28e1af0d45d831
ralphsa95 <ralph_saade@hotmail.com>
2017-08-29 21:45:12 +0300

    Update Controlleur.java

--- Controlleur.java
+++ Controlleur.java
@@ -5,26 +5,27 @@ import java.net.*;
 
 public class Controlleur extends Thread
 {
-	int id;
-	public Controlleur(int i)
-	{
-		this.id = i;
-	}
-	
-	@Override
-	public void run()
-	{
-  	Socket control;
+  int id;
+  public Controlleur(int i)
+  {
+    this.id = i;
+  }
+
+  @Override
+  public void run() 
+  {
+    Socket control;
     PrintWriter out;
     BufferedReader in;
     String message;
     BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
     try
     {
-    	control = new Socket("localhost", 2401);
+      control = new Socket("localhost", 2401);
       out = new PrintWriter(control.getOutputStream(), true);
       in = new BufferedReader(new InputStreamReader(control.getInputStream()));
       String command;
+      System.out.println("Welcome .. Type your commands here:");
       while(true)
       {
       	message = null;
@@ -32,12 +33,10 @@ public class Controlleur extends Thread
       	out.println(command + "/" + id); // send to server
       	message = in.readLine();
         if(message != null)
-        	System.out.println("Server response: " + message);
-        
+          System.out.println("Server response: " + message);
       }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    } catch (Exception e) {
+	e.printStackTrace(); }
+  }
 }
