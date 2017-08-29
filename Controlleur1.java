3cca89ca334489055d5130206157693a940c62de
ralphsa95 <ralph_saade@hotmail.com>
2017-08-29 22:20:47 +0300

    Update Controlleur1.java

--- Controlleur1.java
+++ Controlleur1.java
@@ -1,10 +1,11 @@
 
-public class Controlleur1 {
+public class Controlleur1
+{
 
-	public static void main(String[] args) 
-	{
+  public static void main(String[] args) 
+  {
     Controlleur cont = new Controlleur(1);
     cont.start();
-	}
+  }
 
 }
